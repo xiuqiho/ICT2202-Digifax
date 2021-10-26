8a998e28d26b9966636326661586d3333421b56c
xiuqiho <xiuqiho@gmail.com>
2021-10-26 14:05:24 +0800

    UI Update

---
+++ constants.py
@@ -0,0 +1,28 @@
+"""
+Author:         Ho Xiu Qi
+
+Date Completed: 25th Oct 2021
+Last Updated:   25th Oct 2021
+
+Description:
+> Constants used in the ICT2202 Team Assignment "Digifax" project
+"""
+
+# Dashboard
+SCREEN_WIDTH = 0.95
+SCREEN_HEIGHT = 0.85
+WEBPANEL_WIDTH = 0.65
+SIDEPANEL_WIDTH = 1 - WEBPANEL_WIDTH - 0.055
+LABEL_HEIGHT = 0.05
+LIST_HEIGHT = 1 - LABEL_HEIGHT - 0.055
+
+
+# Relationship data structure
+ADDR = 0
+WEIGHT = 1
+
+# Nodes
+DEFAULT_COLORS = ['#3da831', '#9a31a8', '#3155a8', '#eb4034']
+DEFAULT_WEIGHT = 1
+
+
